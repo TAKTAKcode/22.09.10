6162e8dad2d5fe8adcb43ef0f63bbcd0613c5831
tak <wontaek22@gmail.com>
2022-09-10 21:54:05 +0900

    22.09.10
    Study

---
+++ src/com/tak/study/Study.java
@@ -0,0 +1,13 @@
+package com.tak.study;
+
+public class Study {
+
+    public static void main(String[] args) {
+
+        System.out.println("오늘은 SQLD 자격증 준비를 위해 참고서를 사왔다." +
+                "그리고 code를 짜는 대신 blog에 간단하게 기술 정리글을 작성해놨다." +
+                "이제 겨우 2개 썼지만 하나하나 채워질 모습을 생각하니 뿌듯하다." +
+                "요즘 commit에 너무 sout만 써놔서 이상하니까 얼른 공부를 해야겠다.");
+    }
+
+}
